f5af718a3ba03b29fa41251c85a05244c450edb0
FilmyFather <91774433+FilmyFather@users.noreply.github.com>
2025-02-27 02:54:14 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something For You'.split('')
+const text = 'Hey Switee You Wanna See Something Amazing Likes You...!!!??'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
